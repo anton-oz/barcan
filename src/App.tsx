1f430f49c3d7683cd36cd0eec09bba2b32c18519
anton-oz <antonosland789@gmail.com>
2025-05-04 22:27:38 -0500

    feat: added lucide-react pkg, basic page blocked layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Nav from "./components/nav/Nav";
+import Todo from "./components/todo/Todo";
 
 function App() {
   return (
     <>
       <Nav />
+      <main>
+        <Todo />
+        <section>In progress</section>
+        <section>Done</section>
+      </main>
     </>
   );
 }
